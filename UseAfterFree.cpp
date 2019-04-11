c24092f69060f273ecd214c714fba5bb3e09e2fe
chanhle <letonchanh@gmail.com>
2019-04-11 11:37:55 -0400

    compiled

--- UseAfterFree.cpp
+++ UseAfterFree.cpp
@@ -1,7 +1,14 @@
+#include<stdlib.h>
+
 int f() {
-	char* buf = new char[SIZE];
-    if (true) {
-      free(buf); //error handling has freed the buffer
-    }
-    log_contents(buf); //but it is still used here for logging
+  char* buf = new char[10];
+  if (true) {
+    free(buf); //error handling has freed the buffer
+  }
+  buf[0]; //but it is still used here for logging
+}
+
+int main() {
+  f();
+  return 0;
 }
